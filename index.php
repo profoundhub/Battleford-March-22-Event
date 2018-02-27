d14f7062c9a45ae73853f1a497eb300bae949451
Daniel Lim <hbt@markhamboards.com>
2018-02-26 21:44:02 -0500

    +head + body

--- index.php
+++ index.php
@@ -4,4 +4,97 @@
 ?>
 <!DOCTYPE html>
 <html>
+
+<head>
+  <meta charset="UTF-8">
+  <title>Battleford Corp. | Markham Nexus 2018 Networking Event</title>
+
+  <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+  <link rel="stylesheet" href="css/style.css">
+
+  <style>
+    @import url(/assets/style.css);
+  </style>
+  <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+  <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+
+  <style>
+    .img-upper {
+      width: 260px;
+    }
+
+    #scrollUp {
+        bottom: 20px;
+        right: 20px;
+        padding: 10px 20px;
+        background: #555;
+        color: #fff;        
+        width: 38px;
+        height: 38px;
+        background: url(./img/top.png) no-repeat;
+    }
+
+    #scrollUp-active {
+        display: none;
+    }
+
+    .is-hidden-accessible, .skip-links, .ui-helper-hidden-accessible {
+        position: absolute;
+        left: -999999px;
+    }
+
+  </style>
+  <?php flush(); ?>
+  <!-- Facebook Pixel Code -->
+  <script>
+    ! function(f, b, e, v, n, t, s) {
+      if (f.fbq) return;
+      n = f.fbq = function() {
+        n.callMethod ?
+          n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+      };
+      if (!f._fbq) f._fbq = n;
+      n.push = n;
+      n.loaded = !0;
+      n.version = '2.0';
+      n.queue = [];
+      t = b.createElement(e);
+      t.async = !0;
+      t.src = v;
+      s = b.getElementsByTagName(e)[0];
+      s.parentNode.insertBefore(t, s)
+    }(window,
+      document, 'script', 'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '295481444187735'); // Insert your pixel ID here.
+    fbq('track', 'PageView');
+  </script>
+  <noscript><img height="1" width="1" style="display:none"
+        src="https://www.facebook.com/tr?id=295481444187735&ev=PageView&noscript=1"
+        /></noscript>
+  <!-- DO NOT MODIFY -->
+  <!-- End Facebook Pixel Code -->
+  <script>
+    fbq('track', 'ViewContent', {
+      value: 5.00,
+      currency: 'USD'
+    });
+  </script>
+  <?php /*
+  <script src="//load.sumome.com/" data-sumo-site-id="d1914459ba50257adf83abe8d57e94a01ee29b57ebe4a52cd2f7e927cb049e0d" async="async"></script>
+  */ ?>
+</head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-108355750-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+
+  function gtag() {
+    dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+  gtag('config', 'UA-108355750-1');
+</script>
+
+<body>
+</div> <!-- container ends -->
 </html>
