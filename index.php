7daab71209a246f69f5923503029f1eb02c69f7c
Daniel Lim <hbt@markhamboards.com>
2018-02-26 21:47:09 -0500

    +affiliates

--- index.php
+++ index.php
@@ -384,6 +384,16 @@
     <br />
         
 
+    <div id="affiliates"></div>
+    <h2>Affiliates</h2>
+
+    <div align="center">
+      <a href="https://goo.gl/g9QQNH"><img src="./img/battleford-affiliates.png" style="width: 100%" /></a>
+    </div>
+    <br />
+        <hr />
+    <br />
+  </div> 
 
 </div> <!-- container ends -->
 </html>
