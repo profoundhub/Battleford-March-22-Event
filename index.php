adb07dbfe2a5908eefea8cf058d0e1ef272ac0fa
Daniel Lim <hbt@markhamboards.com>
2018-02-26 21:45:51 -0500

    + sponsors

--- index.php
+++ index.php
@@ -135,6 +135,178 @@
     <br />
         <hr />    
     <br />
-    
+    <div style="width:100%; text-align:left;">
+      <iframe src="https://www.eventbrite.ca/e/nexus-networking-event-tickets-39557894728?ref=eweb" frameborder="0" height="1000" width="100%" vspace="0" hspace="0" marginheight="5" marginwidth="5" scrolling="auto" allowtransparency="true"></iframe>
+      <div style="font-family:Helvetica, Arial; font-size:12px; padding:10px 0 5px; margin:2px; width:100%; text-align:left;">
+        <a class="powered-by-eb" style="color: #ADB0B6; text-decoration: none; position: relative; left: 41%;" target="_blank" href="http://www.eventbrite.ca/">Powered by Eventbrite &amp; Battleford Corp.</a>
+      </div>
+    </div>
+    <br />
+        <hr />    
+    <br />
+    <div id="sponsors"></div>
+    <a href="#">  
+		<img src="./img/BattlefordCorp-Nexus-Event.png" id="avatar" width="500" draggable="false"/>
+    </a>
+    <h2>Sponsors</h2>    
+    <div class="container-fluid" style="background: white;">
+      <div class="row" align="center">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper" style="position: relative; line-height: 272px; width: 260px;">
+              <img src="./sponsors/MasterCard_Logo.png" alt="mastercard logo" width="260" />
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper" style="position: relative; line-height: 275px; width: 260px;">
+              <img src="./sponsors/ART-FOR-ADS-300x200.jpg" alt="art for ads logo" width="260" />
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper">
+              <img src="./sponsors/constantcontactlogo.jpg" alt="constant contact logo" width="260" />
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper" style="position: relative; line-height: 285px; width: 260px;">
+              <img src="./sponsors/BetterBusinessExpo-logo.jpg" alt="Better Business Events Logo" width="260" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row" align="center">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper" style="position: relative; line-height: 180px; width: 260px; height: 100px;">
+              <img src="./sponsors/MBT1.png" alt="tradebank logo" width="260" />
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper" style="position: relative; line-height: 180px; width: 260px; height: 100px;">
+              <img src="./sponsors/Profound-Ideation-Inc-Logo-Sponsor2.png" alt="Profound Ideation Inc. logo" width="260" />
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper" style="position: relative; line-height: 180px; width: 260px; height: 100px;">
+              <img src="./sponsors/microsoft-300x110.png" alt="microsoft" width="260" />
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper" style="padding: 20px 0px 20px;">
+              <img src="./sponsors/MYM_Logo-300x183.png" alt="Make Your Mark Logo" width="260" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row" align="center">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper">
+              <img src="./sponsors/Jewel92_Stacked_notag.jpg" alt="better events logo" width="260" />
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper">
+              <img src="./sponsors/battleford-logo-1.png" alt="Battleford Corp. Inc." width="260" />
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper">
+              <img src="./sponsors/411-logo4.png" alt="411 logo" width="260" />
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper">
+              <img src="./sponsors/MembercloudsClouds-512.png" alt="MemberClouds Logo" width="260" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row" align="center">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper">
+              <img src="./sponsors/MarkhamNexus-logo.png" alt="Markham Nexus Networking Events" width="260" />
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper">
+              <img src="./sponsors/hamilton-Spectator_logo_w350.jpg" alt="Hamilton Spectator – Readers' Choice logo" width="260" />
+            </div> 
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper">
+              <img src="./sponsors/sandler-training-3.png" alt="Sandler Training logo" width="260" />
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper">
+              <img src="./sponsors/Cable14-Now.png" alt="Cable 14 Hamilton logo" width="260" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="row" align="center">
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper">
+              <img src="./sponsors/TradeBank-logo.png" alt="tradebank logo" width="260" />
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper">
+              <img src="./sponsors/metro-logo.png" alt="quick marketing group logo" width="200" />
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper">
+              <img src="./sponsors/logo.png" alt="CaSEO logo" width="260" />
+            </div>
+          </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-6 col-lg-3">
+          <div class="boxes">
+            <div class="img-upper">
+              <img src="./sponsors/Safeguard-Logo-Color-Hi-Res_full.png" alt="safeguard logo" width="260" />
+            </div>
+          </div>
+        </div>
+      </div>
+
+    <br />
+        <hr />
+    <br />    
 </div> <!-- container ends -->
 </html>
